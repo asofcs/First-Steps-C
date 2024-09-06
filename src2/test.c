90802bf89a1f6d1c6063a9175b9d0b9fa7a488b7
asofcs <67157807+asofcs@users.noreply.github.com>
2024-09-06 18:25:18 +0100

    Update test.c

--- src2/test.c
+++ src2/test.c
@@ -1,1 +1,21 @@
+/*
+ * Written by @asofcs
+ * Copyright 2024
+ */
+ 
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    * Display the total number of bytes (size) for different data types       
+    */
+    
+    printf(" \t Bytes number for: \n ");
+    printf(" int: %zd, char: %zd\n ", sizeof(int), sizeof(char));
+    printf(" long: %zd, long long: %zd\n ", sizeof(long), sizeof(long long));
+    printf(" double: %zd,  long double : %zd\n ", sizeof(double), sizeof(long double));
+    
+    return 0;
+}
 
